503712e0009538c3c16e72ca26a8d61801c8d509
fleur <fleurdeveley@gmail.com>
2021-05-26 19:31:58 +0200

    add fixtures and home template

--- app/html/src/DataFixtures/AppFixtures.php
+++ app/html/src/DataFixtures/AppFixtures.php
@@ -3,46 +3,73 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\Picture;
 use App\Entity\Trick;
+use App\Entity\Video;
+use App\Repository\VideoRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
-use Symfony\Component\String\Slugger\SluggerInterface;
+use PhpParser\Node\Stmt\Foreach_;
+use Proxies\__CG__\App\Entity\Category as EntityCategory;
 
 class AppFixtures extends Fixture
 {
-    protected $slugger;
-
-    public function __construct(SluggerInterface $slugger)
-    {
-        $this->slugger = $slugger;
-    }
-
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
-        $faker->addProvider(new \Bezhanov\Faker\Provider\Avatar($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
 
-        for($c = 0; $c < 4; $c++){
+        $categories = [];
+        $categoriesName = ['grabs', 'slides', 'rotations', 'old school'];
+        $tricksName = ['mute', 'nose grab', 'japan', 'slide', 'tail slide', '360°', '720°', '1080°', 'backside air', 'method air'];
+
+        // 4 categories
+        foreach($categoriesName as $categoryName) {
             $category = new Category;
 
-            $category->setName($faker->categoryName);
+            $category->setName($categoryName);
 
             $manager->persist($category);
+            $categories [] = $category;
+        }
 
-            for($t = 0; $t < 15; $t++){
-                $trick = new Trick;
-                $trick->setName($faker->trickName)
-                    ->setContent($faker->paragraph())
-                    ->setMainPicture($faker->imageUrl(400, 400, true))
-                    ->setCreatedAt($faker->dateTime)
-                    ->setUpdateAt($faker->dateTime); 
-            
-                $manager->persist($trick);
+        // 10 tricks
+        foreach($tricksName as $trickName) {
+            $trick = new Trick;
+
+            $trick->setName($trickName)
+                ->setContent($faker->paragraph())
+                ->setMainPicture($faker->imageUrl(400, 400, true))
+                ->setCreatedAt($faker->dateTime)
+                ->setUpdateAt($faker->dateTime)
+                ->setCategory($category);
+
+            $manager->persist($trick);
+
+            // 3 pictures by trick
+            for($p = 0; $p < 3; $p++){
+                $picture = new Picture;
+
+                $picture->setName($trick->getName() . '' . $p . '.jpg')
+                    ->setPath('img/tricks')
+                    ->setTrick($trick);
+                
+                $manager->persist($picture);
+            }
+
+            // 2 videos by trick
+            for($v = 0; $v < mt_rand(1, 2); $v++){
+                $video = new Video;
+
+                $video->setName($trick->getName() . '' . $v . '.jpg')
+                    ->setPath('img/tricks')
+                    ->setTrick($trick);
+                
+                $manager->persist($video);
             }
         }
-        
+
         $manager->flush();
     }
 }
