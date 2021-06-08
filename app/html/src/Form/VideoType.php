34dc478f508d811df36b87d9ac4aecd0f9958ab2
fleur <fleurdeveley@gmail.com>
2021-06-08 08:58:06 +0200

    add VideoType

---
+++ app/html/src/Form/VideoType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Video;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VideoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la vidéo'
+            ])
+            ->add('src', UrlType::class, [
+                'label' => 'Url de la vidéo'
+            ])
+            ->add('trick');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Video::class,
+        ]);
+    }
+}
