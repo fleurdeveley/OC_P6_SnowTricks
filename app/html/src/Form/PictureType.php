742289e6b0a8f4b00486f26ded04d6bed6305c7c
fleur <fleurdeveley@gmail.com>
2021-06-08 08:57:36 +0200

    add PictureType

---
+++ app/html/src/Form/PictureType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Picture;
+use App\Entity\Trick;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PictureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'image'
+            ])
+            ->add('src', FileType::class, [
+                'label' => 'Fichier de l\'image'
+            ])
+            ->add('trick');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Picture::class,
+        ]);
+    }
+}
