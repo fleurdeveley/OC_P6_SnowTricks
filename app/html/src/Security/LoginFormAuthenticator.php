f4d86478c0ca99e722c82ccdd20d6312b4b8f881
fleur <fleurdeveley@gmail.com>
2021-06-24 11:15:41 +0200

    deleting use PasswordAutheticatedUserInterface

--- app/html/src/Security/LoginFormAuthenticator.php
+++ app/html/src/Security/LoginFormAuthenticator.php
@@ -13,7 +13,6 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Csrf\CsrfToken;
