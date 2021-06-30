10e742df304d969e13ecb4ee22727dd3f551f656
fleur <fleurdeveley@gmail.com>
2021-06-30 09:49:03 +0200

    verification of the activation of the mail during registration

--- app/html/src/Security/LoginFormAuthenticator.php
+++ app/html/src/Security/LoginFormAuthenticator.php
@@ -75,11 +75,14 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             throw new InvalidCsrfTokenException();
         }
 
-        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $credentials['email']]);
+        $user = $this->entityManager->getRepository(User::class)->findOneBy([
+            'email' => $credentials['email'],
+            'activated' => true
+        ]);
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('Email introuvable.');
         }
 
         return $user;
